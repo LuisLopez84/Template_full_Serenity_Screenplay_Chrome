422c34346d7db33ffe8198db58fe75ee33acd905
Luis Lopez <ingluislopezh@outlook.com>
2025-06-04 15:42:25 -0500

    runner 2

--- src/test/java/co/com/automation/testing/runner/CucumberTestSuite.java
+++ src/test/java/co/com/automation/testing/runner/CucumberTestSuite.java
@@ -4,10 +4,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 
 
