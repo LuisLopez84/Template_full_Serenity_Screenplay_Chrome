0419e43635da1a48f4d2b5b5acc551d4fa656bd6
Luis Lopez <ingluislopezh@outlook.com>
2025-07-11 07:45:10 -0500

    Proyecto corriendo en 3 escenarios

--- src/main/java/co/com/automation/testing/questions/ValidarUsuarioConsultado.java
+++ src/main/java/co/com/automation/testing/questions/ValidarUsuarioConsultado.java
@@ -1,12 +1,29 @@
 package co.com.automation.testing.questions;
 
 import co.com.automation.testing.ui.ConsultarUsuarioPage;
+import co.com.automation.testing.ui.CrearUsuarioPage;
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.ensure.Ensure;
 import net.serenitybdd.screenplay.questions.Text;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
-public class ValidarUsuarioConsultado {
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
-    public static Question<String> name() {
-        return actor -> Text.of(ConsultarUsuarioPage.EMPLOYEE_NAME_RESULT).answeredBy(actor);
+public class ValidarUsuarioConsultado implements Question<Boolean> {
+
+    @Override
+    public Boolean answeredBy(Actor actor) {
+        actor.attemptsTo(
+                WaitUntil.the(ConsultarUsuarioPage.SUCCESS_MESSAGE, isVisible()).forNoMoreThan(10).seconds(),
+                Ensure.that(ConsultarUsuarioPage.SUCCESS_MESSAGE).isDisplayed(),
+                Ensure.that(ConsultarUsuarioPage.SUCCESS_MESSAGE).text().contains("LLOPEZ5")
+        );
+        return true;
+    }
+
+    public static ValidarUsuarioConsultado validacionUsuarioConsultado () {
+
+        return new ValidarUsuarioConsultado();
     }
 }
